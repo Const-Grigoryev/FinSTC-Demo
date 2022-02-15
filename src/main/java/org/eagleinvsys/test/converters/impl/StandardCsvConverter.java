e30ab56b27e17a2ff17552e33ba4cfa4b694264e
Constantine <aspid812@gmail.com>
2022-02-15 23:01:52 +0300

    Add StandardCsvConverter implementation and test

--- src/main/java/org/eagleinvsys/test/converters/impl/StandardCsvConverter.java
+++ src/main/java/org/eagleinvsys/test/converters/impl/StandardCsvConverter.java
@@ -1,12 +1,44 @@
 package org.eagleinvsys.test.converters.impl;
 
+import org.eagleinvsys.test.converters.ConvertibleCollection;
+import org.eagleinvsys.test.converters.ConvertibleMessage;
 import org.eagleinvsys.test.converters.StandardConverter;
 
 import java.io.OutputStream;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class StandardCsvConverter implements StandardConverter {
+
+    static ConvertibleMessage adaptConvertibleMessage(Map<String, String> record) {
+        return record::get;
+    }
+
+    static ConvertibleCollection adaptConvertibleCollection(List<Map<String, String>> records) {
+        return new ConvertibleCollection() {
+            @Override
+            public Collection<String> getHeaders() {
+                if (!records.isEmpty()) {
+                    return records.get(0).keySet();
+                }
+                else {
+                    return Collections.emptySet();
+                }
+            }
+
+            @Override
+            public Iterable<ConvertibleMessage> getRecords() {
+                return () -> records.stream()
+                        .map(StandardCsvConverter::adaptConvertibleMessage)
+                        .iterator();
+            }
+        };
+    }
+
 
     private final CsvConverter csvConverter;
 
@@ -22,7 +54,7 @@ public class StandardCsvConverter implements StandardConverter {
      */
     @Override
     public void convert(List<Map<String, String>> collectionToConvert, OutputStream outputStream) {
-        // TODO: implement by using csvConverter
+        csvConverter.convert(adaptConvertibleCollection(collectionToConvert), outputStream);
     }
 
 }
--- src/test/java/org/eagleinvsys/test/converters/impl/StandardCsvConverterTests.java
+++ src/test/java/org/eagleinvsys/test/converters/impl/StandardCsvConverterTests.java
@@ -1,7 +1,65 @@
 package org.eagleinvsys.test.converters.impl;
 
+import org.eagleinvsys.test.converters.ConvertibleMessage;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 public class StandardCsvConverterTests {
 
-    // TODO: implement JUnit 5 tests for StandardCsvConverter
+    private static void assertEquals(Map<String, String> expected, ConvertibleMessage actual, Collection<String> headers) {
+        for (var header : headers) {
+            Assert.assertEquals(expected.get(header), actual.getElement(header));
+        }
+    }
 
+    @Test
+    public void adaptConvertibleMessageTest() {
+        var headers = Set.of("name", "date_of_birth", "date_of_death", "team");
+
+        var original = Map.of(
+                "name", "Lewis Hamilton",
+                "date_of_birth", "7 January 1985",
+                "team", "Mercedes"
+        );
+
+        var adapted = StandardCsvConverter.adaptConvertibleMessage(original);
+
+        assertEquals(original, adapted, headers);
+    }
+
+    @Test
+    public void adaptConvertibleCollectionTest() {
+        var headers = Set.of("name", "date_of_birth", "team");
+
+        var original = List.of(
+                Map.of(
+                        "name", "Lewis Hamilton",
+                        "date_of_birth", "7 January 1985",
+                        "team", "Mercedes"
+                ),
+                Map.of(
+                        "name", "Ayrton Senna",
+                        "date_of_birth", "21 March 1960",
+                        "team", "McLaren"
+                )
+        );
+
+        var adapted = StandardCsvConverter.adaptConvertibleCollection(original);
+        var adaptedRecords = StreamSupport
+                .stream(adapted.getRecords().spliterator(), false)
+                .collect(Collectors.toUnmodifiableList());
+
+        Assert.assertEquals(headers, adapted.getHeaders());
+
+        Assert.assertEquals(original.size(), adaptedRecords.size());
+        assertEquals(original.get(0), adaptedRecords.get(0), headers);
+        assertEquals(original.get(1), adaptedRecords.get(1), headers);
+    }
 }
